1d0866c52a2a1b4817ffb6ce8d80f95ef4a5df9b
nicolas.fischer@belarion.de <kommtaufdiepommensochwasdrauf>
2021-08-23 16:58:31 +0200

    scriptInit

--- setup.sh
+++ setup.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-echo "Setting up the Apple Magic Trackpad 2 Linux on you Linux System"
+printf "\n\n\nApple Magic Trackpad 2 Linux Setup\n"
+printf "==================================================\n"
+if ! xinput list | grep 'Apple Inc. Magic Trackpad 2'
+then
+	printf "Touchpad not found\n"
+	exit
+fi
+printf  "Kernel Version (>4.20 needed):"
+uname -r
+id=$(xinput list | grep 'Apple Inc. Magic Trackpad 2i2' | sed 's/.*id=//' |sed 's/\s.*$//')
+if ! command -v synclient &> /dev/null
+then
+    echo "This software needs the synclient tool"
+    echo "make sure you have it installed"
+    exit
+fi
+
+
+synclient
